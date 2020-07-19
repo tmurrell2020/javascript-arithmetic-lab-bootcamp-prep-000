eff0115dbf050660cfe41d1e6f0d3f8d86888364
Terrell Murrell <littlerell12@gmail.com>
2020-07-19 16:29:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,32 +16,6 @@ function divide() {
 
 var number = 10
 
-function add5() {
-  number += 5
-}
-
-function divideBy3() {
-  number /= 3
-}
-
-divideBy3()
-
-console.log(number)
-
-add5()
-
-console.log(number)
-
-number = 10
-
-add5()
-
-console.log(number)
-
-divideBy3()
-
-console.log(number)
-
 function increment(n) {
   number += 1
 }
