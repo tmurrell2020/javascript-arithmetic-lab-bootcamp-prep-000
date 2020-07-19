42f05b8dc4c1ad0e5688e94f210bdc901754390a
Terrell Murrell <littlerell12@gmail.com>
2020-07-19 16:27:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,39 @@ function multiply() {
 function divide() {
   5.0 / 2.5
 }
+
+var number = 10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+number = 10
+
+add5()
+
+console.log(number)
+
+divideBy3()
+
+console.log(number)
+
+function increment(n) {
+  number += 1
+}
+
+function decrement(n) {
+  number -= 1
+}
